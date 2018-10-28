510c55ffc82049ff8a3413457a678c2d7ffccbf0
yangxq5858 <yangxq5858@qq.com>
2018-10-28 22:09:41 +0800

    xx

--- spring-cloud/micro-weather-config-client/src/test/java/com/hxcoltd/weather/WeatherApplicationTests.java
+++ spring-cloud/micro-weather-config-client/src/test/java/com/hxcoltd/weather/WeatherApplicationTests.java
@@ -1,25 +1,24 @@
 package com.hxcoltd.weather;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.assertEquals;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WeatherApplicationTests {
 
-    @Test
-    public void contextLoads() {
-    }
-
     @Value("${auther}")
     private String auther;
+
     @Test
-    public void testConfigFromCenter(){
-        assertEquals("yxqiang",auther);
+    public void contextLoads() {
+        System.out.println(auther);
+        assertEquals("yxqiang", auther);
     }
+
 }
